9066e5bb9f13221dc68bb18f5b47fd209396cbd2
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-24 09:16:50 -0700

    create users and github_users tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS github_users;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE github_users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT
+    avatar TEXT
+)
