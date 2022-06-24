9f133e0261aa29ae99dbf6b2c6f9c9ceee5c1e07
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-24 09:42:21 -0700

    added github client id, secret, and redirect_uri

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,19 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS github_users;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password_hash TEXT NOT NULL
-);
+-- CREATE TABLE users (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     first_name TEXT NOT NULL,
+--     last_name TEXT NOT NULL,
+--     email TEXT NOT NULL,
+--     password_hash TEXT NOT NULL
+-- );
+
+DROP TABLE IF EXISTS github_users;
 
 CREATE TABLE github_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username TEXT NOT NULL,
-    email TEXT
+    email TEXT,
     avatar TEXT
-)
+);
